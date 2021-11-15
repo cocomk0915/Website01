127bfe3ffab9060508acc27b7312130c9efb6863
Minkeun Cho <mcho9@hawk.iit.edu>
2021-11-15 15:46:59 -0600

    Work on Step 3.

--- pp-04/site.js
+++ pp-04/site.js
@@ -11,8 +11,8 @@ function double(num) {
   return x;
 }
 
-double(6);
-console.log('The value of x is', x, '-- it should be 5.');
+double(1);
+console.log('The Value of x is', x, '-- it should be 5.');
 
 /*
   Step 2. Rewrite the JavaScript `double()` function above so that
@@ -23,16 +23,36 @@ console.log('The value of x is', x, '-- it should be 5.');
   Be sure to comment out the original `double()` function so you
   can accurately test your work.
 */
-( function()) {
+(function() {
   var x = 5;
   function double (num) {
     var x = num * 2;
     return x;
   }
+
+double (1);
+console.log('The Value of x is', x, '-- it should be 5.');
 }
+);
 
 /*
   Step 3. Rewrite your corrected `double()` function from Step 2.
   so that non-number values passed into the function are handled
   in some reasonable way.
 */
+
+(function() {
+  var x = 5;
+  function double (num) {
+    if (typeof(num) = "number") {
+      var x = num * 2;
+      return x;
+    }
+    else{
+      return ('Input is not a correct number.')
+    }
+  }
+
+  double (1);
+  console.log('The Value of x is', x, '-- it should be 5.');
+})();
